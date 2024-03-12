dbe86e7aaa36823bdf3cabed0aa43326301a60a2
marc <khourymark1@gmail.com>
2024-03-12 22:13:07 +0200

    2nd commit - Calling Students Endpoint

--- index.js
+++ index.js
@@ -2,3 +2,20 @@ function test()
 {
     alert('marc');
 }
+
+function RenderStudents()
+{
+    let ddl_students = document.getElementById('ddl_students');
+    fetch('https://practice20240311.onrender.com/students')
+      .then(response => response.json())
+      .then(data=> {
+        data.forEach(student => {
+            let option = document.createElement('option')
+            option.innerHTML = student.name;
+            ddl_students.appendChild(option);
+            
+            });
+      })
+}
+
+RenderStudents();
